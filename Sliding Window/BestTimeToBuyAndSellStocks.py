494a183b256df38f51e32b28387c03433d1c15b7
Sotaro Ito <sotaro.univ@gmail.com>
2026-04-13 16:30:16 +0900

    enter sliding window section

---
+++ Sliding Window/BestTimeToBuyAndSellStocks.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        profit = 0
+        max_profit = 0
+
+        for i in range(len(prices)):
+            min_price = min(min_price, prices[i])
+            profit = prices[i] - min_price
+
+            max_profit = max(max_profit, profit)
+
+        return max_profit
